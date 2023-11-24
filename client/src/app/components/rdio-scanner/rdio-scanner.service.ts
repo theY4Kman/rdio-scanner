bcf0f99dde0d70318115cd9c0a07d61fd8073378
Zach Kanzler <they4kman@gmail.com>
2023-11-24 04:06:32 -0500

    fix(client): clear queue duration after stopping live feed

--- client/src/app/components/rdio-scanner/rdio-scanner.service.ts
+++ client/src/app/components/rdio-scanner/rdio-scanner.service.ts
@@ -650,7 +650,7 @@ export class RdioScannerService implements OnDestroy {
 
         this.clearQueue();
 
-        this.event.emit({ livefeedMode: this.livefeedMode, queue: 0 });
+        this.event.emit({ livefeedMode: this.livefeedMode, queue: 0, queueDuration: 0 });
 
         this.stop();
 
